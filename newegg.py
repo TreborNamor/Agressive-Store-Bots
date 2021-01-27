2e9c4dbe0775df2abf828298b01959bc23937891
Robert Roman <robertroman7@gmail.com>
2021-01-27 17:03:18 -0600

    updated geckodriver manager

--- newegg.py
+++ newegg.py
@@ -10,6 +10,7 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.firefox.options import Options
 import random
 
