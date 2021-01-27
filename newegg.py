abde8e028607be5e40dcb8a06acf54df1a7eace8
Robert Roman <robertroman7@gmail.com>
2021-01-27 15:47:23 -0600

    added Options module

--- newegg.py
+++ newegg.py
@@ -10,6 +10,7 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.options import Options
 import random
 
 # Newegg credentials
